87fa399c0045b01cfdd44345cf638228691b2c16
Joël Gaujard <j.gaujard@gmail.com>
2020-05-08 17:32:10 +0200

    First challenge

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,23 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+  ];
+
+  List<String> questions = [
+    'Une vache peut-elle descendre des marches mais pas les monter.',
+    'À peu près un quart des os d\'un humain se trouve dans les pieds.',
+    'Le sang d\'une limace est vert.'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +54,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions.first,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -84,7 +101,9 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
